b83670fa13bfbc240eb6bc62714bfb645af9955a
Hans Rivera <hrivera@unal.edu.co>
2022-01-29 16:29:29 -0500

    configurated tailwind to use custom themes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: { "card-background": "#28272B", "principal-button": "#FFA049" },
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
+  },
+  plugins: [],
+};
