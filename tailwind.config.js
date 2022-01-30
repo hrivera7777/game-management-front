9fa3dfd79fad8b8318e9393eec79cbbc2d5815eb
Hans Rivera <hrivera@unal.edu.co>
2022-01-30 18:56:08 -0500

    added custom breakpoints and custom page color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
   theme: {
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1350px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1920px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       colors: {
         "principal-background-color": "#28272B",
         "principal-color": "#FFA049",
         "icon-background": "#46444B",
+        "pages-background": "#3C3A40",
       },
     },
     fontFamily: {
