89f697206a9956373d9acace34093d7f62f7e867
Hans Rivera <hrivera@unal.edu.co>
2022-01-30 18:52:22 -0500

    updated navbar sections to make it responsive

--- src/Components/Navbar/NavBarSectionItem.js
+++ src/Components/Navbar/NavBarSectionItem.js
@@ -5,14 +5,12 @@ class NavBarSectionItem extends Component {
     return (
       <div>
         <div className="my-5 ml-10 flex flex-row">
-          <div className="mr-8 rounded-full bg-icon-background p-2">
-            <img
-              src={this.props.itemLogo}
-              alt={`logo-${this.props.itemName}`}
-            />
-          </div>
+          <img
+            className="mr-8 rounded-full bg-icon-background p-2"
+            src={this.props.itemLogo}
+            alt={`logo-${this.props.itemName}`}
+          />
           <h3 className="mr-16 p-2 text-lg text-white">
-            {" "}
             {this.props.itemName}
           </h3>
         </div>
