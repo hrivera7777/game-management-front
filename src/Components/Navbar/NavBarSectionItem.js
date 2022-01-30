5c21b70bfb29bd898e64685da6559d16ad198be4
Hans Rivera <hrivera@unal.edu.co>
2022-01-29 19:17:22 -0500

    added rounded icons

--- src/Components/Navbar/NavBarSectionItem.js
+++ src/Components/Navbar/NavBarSectionItem.js
@@ -4,9 +4,17 @@ class NavBarSectionItem extends Component {
   render() {
     return (
       <div>
-        <div className="flex flex-row">
-          <img src={this.props.itemLogo} alt={`logo-${this.props.itemName}`} />
-          <h3> {this.props.itemName}</h3>
+        <div className="my-5 ml-10 flex flex-row">
+          <div className="mr-8 rounded-full bg-icon-background p-2">
+            <img
+              src={this.props.itemLogo}
+              alt={`logo-${this.props.itemName}`}
+            />
+          </div>
+          <h3 className="mr-16 p-2 text-lg text-white">
+            {" "}
+            {this.props.itemName}
+          </h3>
         </div>
       </div>
     );
