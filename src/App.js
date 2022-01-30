780538814cb05ae4098fe5ef997705da740bbb56
Hans Rivera <hrivera@unal.edu.co>
2022-01-30 18:57:24 -0500

    added navbar and grid to test them on a page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import NavBar from "./Layout/Navbar";
+import Grid from "./Layout/Grid";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <div className="flex flex-row bg-pages-background">
+        <NavBar />
+        <Grid />
+      </div>
+    </div>
+  );
 }
 
 export default App;
