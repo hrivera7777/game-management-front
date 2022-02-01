79dce580438762eac6e2db7701b8e672d386c69d
Hans Rivera <hrivera@unal.edu.co>
2022-01-31 19:26:04 -0500

    Updated button component to construct dynamic class names following the tailwind docs
    https://tailwindcss.com/docs/content-configuration\#class-detection-in-depth

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,8 +3,11 @@ import { Component } from "react";
 class Button extends Component {
   render() {
     const { logo, text, color } = this.props;
+    const tailwindColor = "bg-" + color;
     return (
-      <button className={`flex flex-row rounded-full bg-${color} py-2 px-5`}>
+      <button
+        className={`${tailwindColor} flex flex-row rounded-full py-2 px-5`}
+      >
         <img className="mr-3" src={logo} alt="card-image" />
         <h2 className="py-3 text-white">{text}</h2>
       </button>
