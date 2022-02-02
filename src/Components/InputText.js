3a3cd41ad8b4d0bccc18bf1b64056be728a2ad3c
Hans Rivera <hrivera@unal.edu.co>
2022-02-02 12:31:55 -0500

    change static identifier to a dynamic one to fix wrong label behavior

--- src/Components/InputText.js
+++ src/Components/InputText.js
@@ -5,16 +5,16 @@ class InputText extends Component {
     console.log(event.target.value);
   }
   render() {
-    const { labelName, logo } = this.props;
+    const { labelName, logo, type } = this.props;
     return (
-      <label htmlFor="inputText" className="flex flex-col text-white">
+      <label htmlFor={labelName} className="flex flex-col text-white">
         {labelName}
-        <div className="mt-3 flex w-fit flex-row rounded-full bg-second-gray p-2">
+        <div className="mt-3 mb-3 flex h-12 w-fit flex-row rounded-full bg-second-gray p-2">
           <img src={logo} alt={`${labelName}`} className="mr-2" />
           <input
-            type="text"
-            name="inputText"
-            id="inputText"
+            type={type}
+            name={labelName}
+            id={labelName}
             onChange={this.handleChange}
             className="rounded-r-full bg-second-gray text-white focus:outline focus:outline-1 focus:outline-white"
           />
