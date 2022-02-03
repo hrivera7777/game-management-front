595273bb4b8f299ccadb41dbd9873bd0878ed85b
Hans Rivera <hrivera@unal.edu.co>
2022-02-03 13:44:53 -0500

    Updated css styles in both of the reports to follow the new report page layout

--- src/Components/report/MostAddedGameCard.js
+++ src/Components/report/MostAddedGameCard.js
@@ -4,10 +4,9 @@ import FavoriteCard from "../Favorite/FavoriteCard";
 class MostAddedGameCard extends Component {
   render() {
     return (
-      <div>
-        <div className="flex max-w-5xl flex-col rounded-lg bg-icon-background">
-          <div className="w-full rounded-t-lg bg-principal-background px-12 pb-4">
-            <h2 className="mx-auto mt-4 w-fit text-3xl font-bold text-white">
+      <div className="mb-4 h-fit w-full max-w-5xl rounded-lg bg-principal-background pt-8 desktop:max-w-8xl largeDesktop:max-w-full">
+        <div className="w-full px-12 pb-4">
+          <h2 className="mx-auto w-fit rounded-full border-4 border-dotted border-principal-orange py-3 px-6 text-3xl font-bold text-white">
               Most Added Game Among Favorite Lists
             </h2>
             <h2 className="mt-8 text-xl font-bold text-principal-orange">
