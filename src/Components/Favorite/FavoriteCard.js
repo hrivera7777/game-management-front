3b8b7d8ddf6e2a8b5ed5b3e8c13761fb7e72ed8a
Hans Rivera <hrivera@unal.edu.co>
2022-02-02 19:00:37 -0500

    changed repetive margin to use padding

--- src/Components/Favorite/FavoriteCard.js
+++ src/Components/Favorite/FavoriteCard.js
@@ -7,14 +7,12 @@ class FavoriteCard extends Component {
   render() {
     return (
       <div>
-        <div className="flex max-w-xs flex-col rounded-lg bg-principal-background">
-          <h2 className="mx-8 mt-8 text-2xl font-bold text-white">
+        <div className="flex max-w-xs flex-col rounded-lg bg-principal-background px-8">
+          <h2 className="mt-8 text-2xl font-bold text-white">
             Favorite name list
           </h2>
-          <h2 className="mx-8 mt-4 text-2xl font-bold text-white">
-            Description:
-          </h2>
-          <p className="mx-8 mt-2 text-justify font-poppins text-sm font-medium text-white">
+          <h2 className="mt-4 text-2xl font-bold text-white">Description:</h2>
+          <p className="mt-2 text-justify font-poppins text-sm font-medium text-white">
             Lorem ipsum dolor sit amet consectetur adipiscing elit morbi,
             quisque massa tellus vivamus nec natoque pharetra, diam faucibus
             ultricies tristique.
