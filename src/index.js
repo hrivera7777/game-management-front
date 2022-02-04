f6bec1104f580f725d2ba5ffb735b53754f76c52
Hans Rivera <hrivera@unal.edu.co>
2022-02-03 23:11:10 -0500

    Added the routes that exists in the navbar component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Reports from "./pages/Reports";
 
 const app = document.getElementById("root");
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="games" element={<Home type="game" />} />
+      <Route path="favorites" element={<Home type="favorite" />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="reports" element={<Reports />} />
+    </Routes>
+  </BrowserRouter>,
+  app
+);
