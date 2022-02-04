98fbf6910a919b62b2b7f71843a74a8c61963da9
Hans Rivera <hrivera@unal.edu.co>
2022-02-03 23:08:44 -0500

    Changed the navbar section items to be a link react tag instead of a raw div

--- src/Components/Navbar/NavBarSectionItem.js
+++ src/Components/Navbar/NavBarSectionItem.js
@@ -1,20 +1,21 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NavBarSectionItem extends Component {
   render() {
+    const { itemLogo, itemName } = this.props;
     return (
-      <div>
-        <div className="my-5 ml-10 flex flex-row">
-          <img
-            className="mr-8 rounded-full bg-icon-background p-2"
-            src={this.props.itemLogo}
-            alt={`logo-${this.props.itemName}`}
-          />
-          <h3 className="mr-16 p-2 text-lg text-white">
-            {this.props.itemName}
-          </h3>
-        </div>
-      </div>
+      <Link
+        to={`/${itemName.replace(/\s+/g, "-").toLowerCase()}`}
+        className="my-5 ml-10 flex flex-row"
+      >
+        <img
+          className="max-w-10 mr-8 max-h-10 rounded-full bg-icon-background p-2"
+          src={itemLogo}
+          alt={`logo-${itemName}`}
+        />
+        <h3 className="mr-16 p-2 text-lg text-white">{itemName}</h3>
+      </Link>
     );
   }
 }
