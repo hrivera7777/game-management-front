5a1e27755c6c1b4312c65f012e2670d36b355f7a
Hans Rivera <hrivera@unal.edu.co>
2022-02-04 18:59:10 -0500

    Created modal content dynamictly
    - added edit content with different inputs depends of the category
    - added delete description
    - added favorite description

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,29 +1,86 @@
 import { Component } from "react/cjs/react.development";
 import { Dialog, Transition, Fragment } from "@headlessui/react";
+import InputIcons from "./InputIcons";
+import InputText from "./InputText";
+
+function EditFields(props) {
+  const { inputs, category } = props;
+  return inputs[category].map((inputProperties, index) => {
+    return <InputText {...inputProperties} key={index} />;
+  });
+}
+
+function DeleteDescription() {
+  return (
+    <p className="text-sm text-white">Are you sure to delete --somethig--</p>
+  );
+}
+
+function FavoriteDescription() {
+  return (
+    <p className="text-sm text-white">Do you want to add --somethig-- :)</p>
+  );
+}
+
+function SelectDescription(props) {
+  const { type, inputs, category } = props;
+
+  const selection = {
+    edit: <EditFields inputs={inputs} category={category} />,
+    delete: <DeleteDescription />,
+    favorite: <FavoriteDescription />,
+  };
+  return <div className="mt-2">{selection[type]}</div>;
+}
 
 function ModalContent(props) {
-  return (
-    <div className="inline-block h-full w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-      <Dialog.Title
-        as="h3"
-        className="text-lg font-medium leading-6 text-gray-900"
-      >
-        Payment successful
-      </Dialog.Title>
-      <div className="mt-2">
-        <p className="text-sm text-gray-500">
-          Your payment has been successfully submitted. We've sent you an email
-          with all of the details of your order.
-        </p>
-      </div>
+  const { type, toggleModal, category, name } = props;
+  const inputs = {
+    game: [
+      { logo: InputIcons.inputLogo, labelName: "Name", type: "text" },
+      { logo: InputIcons.genreLogo, labelName: "Genre", type: "text" },
+      { logo: InputIcons.priceLogo, labelName: "Price", type: "number" },
+      { logo: InputIcons.fileLogo, labelName: "File", type: "file" },
+    ],
+    favorite: [
+      { logo: InputIcons.inputLogo, labelName: "Name", type: "text" },
+      {
+        logo: InputIcons.descriptionLogo,
+        labelName: "Description",
+        type: "text",
+      },
+    ],
+  };
+  const buttonOptions = {
+    accept: "Yep!",
+    cancel: "Cancel",
+  };
 
-      <div className="mt-4">
+  return (
+    <div className="inline-block h-full w-full max-w-md transform overflow-hidden rounded-2xl bg-principal-background px-8 py-4 text-left align-middle shadow-xl transition-all">
+      <Dialog.Title
+        as="h2"
+        className="text-lg font-medium leading-6 text-white"
+      >
+        {name}
+      </Dialog.Title>
+
+      <SelectDescription type={type} inputs={inputs} category={category} />
+
+      <div className="mt-4 flex flex-row justify-start">
         <button
           type="button"
-          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-          onClick={props.toggleModal}
+          className="mr-4 rounded-md border border-transparent bg-principal-purple px-4 py-2 text-sm font-medium text-white hover:bg-secondary-purple focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          onClick={toggleModal}
         >
-          Got it, thanks!
+          {buttonOptions["accept"]}
+        </button>
+        <button
+          type="button"
+          className="rounded-md border border-transparent bg-principal-red px-4 py-2 text-sm font-medium text-white hover:bg-secondary-red focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          onClick={toggleModal}
+        >
+          {buttonOptions["cancel"]}
         </button>
       </div>
     </div>
@@ -32,7 +89,7 @@ function ModalContent(props) {
 
 class Modal extends Component {
   render() {
-    const { showModal, toggleModal } = this.props;
+    const { showModal, toggleModal, type, category, name } = this.props;
     return (
       <Dialog
         as="div"
@@ -43,7 +100,12 @@ class Modal extends Component {
         <div className="flex min-h-screen items-center justify-center">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
 
-          <ModalContent toggleModal={toggleModal}></ModalContent>
+          <ModalContent
+            toggleModal={toggleModal}
+            type={type}
+            category={category}
+            name={name}
+          ></ModalContent>
         </div>
       </Dialog>
     );
