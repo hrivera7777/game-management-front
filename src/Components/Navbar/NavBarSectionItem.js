52c6b1c08664613d1e51316bee860126299e86ac
Hans Rivera <hrivera@unal.edu.co>
2022-02-04 19:06:53 -0500

    added tailwind styles on navbar items to be interactive

--- src/Components/Navbar/NavBarSectionItem.js
+++ src/Components/Navbar/NavBarSectionItem.js
@@ -7,14 +7,16 @@ class NavBarSectionItem extends Component {
     return (
       <Link
         to={`/${itemName.replace(/\s+/g, "-").toLowerCase()}`}
-        className="my-5 ml-10 flex flex-row"
+        className="group my-5 ml-10 flex flex-row"
       >
         <img
-          className="max-w-10 mr-8 max-h-10 rounded-full bg-icon-background p-2"
+          className="max-w-10 mr-2 max-h-10 rounded-full bg-icon-background p-2 group-hover:bg-pages-background group-active:bg-principal-orange"
           src={itemLogo}
           alt={`logo-${itemName}`}
         />
-        <h3 className="mr-16 p-2 text-lg text-white">{itemName}</h3>
+        <h3 className="mr-16 p-2 text-lg text-gray-500 group-hover:text-white group-active:text-white">
+          {itemName}
+        </h3>
       </Link>
     );
   }
