dc04c82afd12ed04396d614a1f9eaa81480f7467
Hans Rivera <hrivera@unal.edu.co>
2022-02-06 01:10:15 -0500

    changed link to NavLink to use active state and change tailwind properties

--- src/Components/Navbar/NavBarSectionItem.js
+++ src/Components/Navbar/NavBarSectionItem.js
@@ -1,23 +1,39 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 class NavBarSectionItem extends Component {
   render() {
+    const activeTextStyle = "text-white";
+    const activeIconStyle = "bg-principal-orange";
     const { itemLogo, itemName } = this.props;
     return (
-      <Link
+      <NavLink
         to={`/${itemName.replace(/\s+/g, "-").toLowerCase()}`}
         className="group my-5 ml-10 flex flex-row"
       >
-        <img
-          className="max-w-10 mr-2 max-h-10 rounded-full bg-icon-background p-2 group-hover:bg-pages-background group-active:bg-principal-orange"
-          src={itemLogo}
-          alt={`logo-${itemName}`}
-        />
-        <h3 className="mr-16 p-2 text-lg text-gray-500 group-hover:text-white group-active:text-white">
-          {itemName}
-        </h3>
-      </Link>
+        {({ isActive }) => (
+          <div className="flex flex-row">
+            <img
+              className={`${
+                isActive
+                  ? activeIconStyle
+                  : "bg-icon-background group-hover:bg-pages-background"
+              } max-w-10 mr-2 max-h-10 rounded-full p-2`}
+              src={itemLogo}
+              alt={`logo-${itemName}`}
+            />
+            <h3
+              className={`${
+                isActive
+                  ? activeTextStyle
+                  : "text-gray-500 group-hover:text-white"
+              } mr-16 p-2 text-lg`}
+            >
+              {itemName}
+            </h3>
+          </div>
+        )}
+      </NavLink>
     );
   }
 }
