3f4b84603cb2923226461f521f32212057bd036c
Hans Rivera <hrivera@unal.edu.co>
2022-02-06 13:00:16 -0500

    added method to obtain whole favorite list and set as state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,40 @@ import NavBar from "../Layout/Navbar";
 import Grid from "../Layout/Grid";
 
 class Home extends Component {
-  state = { games: [] };
-  async componentDidMount() {
+  state = { games: [], favorites: [] };
+
+  async getGamesCatalog() {
     const res = await fetch(`http://localhost:3000/game`);
     const json = await res.json();
     this.setState({ games: json });
   }
+
+  async getFavoriteList() {
+    const authType = "Bearer";
+    const res = await fetch("http://localhost:3000/favorite", {
+      method: "GET",
+      headers: {
+        Authorization: authType.concat(" ", localStorage.getItem("token")),
+      },
+      mode: "cors",
+    });
+
+    const json = await res.json();
+    this.setState({ favorites: json });
+  }
+
+  async componentDidMount() {
+    await this.getGamesCatalog();
+    await this.getFavoriteList();
+  }
+
   render() {
     const { category } = this.props;
-    const { games } = this.state;
+    const { games, favorites } = this.state;
     return (
       <div className="flex flex-row bg-pages-background">
         <NavBar />
-        <Grid category={category} games={games} />
+        <Grid category={category} games={games} favorites={favorites} />
       </div>
     );
   }
