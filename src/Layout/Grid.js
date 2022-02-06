cc794c344932349637df82375cf8b32e25135c4a
Hans Rivera <hrivera@unal.edu.co>
2022-02-06 13:01:51 -0500

    added new component to manage catalog of favorite cards

--- src/Layout/Grid.js
+++ src/Layout/Grid.js
@@ -13,24 +13,49 @@ function CatalogGameCard(props) {
   });
 }
 
+function CatalogFavoriteCard(props) {
+  const { complete, category, favorites } = props;
+  return favorites.map((favorite, index) => {
+    return (
+      <FavoriteCard
+        {...favorite}
+        complete={complete}
+        category={category}
+        key={index}
+      />
+    );
+  });
+}
+
 function SelectCard(props) {
-  const { category, complete, games } = props;
+  const { category, complete, games, favorites } = props;
   const cardTypes = {
     game: (
       <CatalogGameCard games={games} complete={complete} category={category} />
     ),
-    favorite: <FavoriteCard complete={complete} category={category} />,
+    favorite: (
+      <CatalogFavoriteCard
+        complete={complete}
+        category={category}
+        favorites={favorites}
+      />
+    ),
   };
   return cardTypes[category];
 }
 class Grid extends Component {
   render() {
-    const { category, games } = this.props;
+    const { category, games, favorites } = this.props;
     return (
       <div className="mx-auto block min-h-screen">
         <div className="my-4 flex flex-col gap-4 px-4 tablet:grid tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-5 desktop:gap-3">
           <CreateCard category={category} />
-          <SelectCard category={category} complete={true} games={games} />
+          <SelectCard
+            category={category}
+            complete={true}
+            games={games}
+            favorites={favorites}
+          />
         </div>
       </div>
     );
